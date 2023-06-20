4a4207bb289ac77ac49dcdee3e6d195ee31fef71
incretech23 <incretechpvtltd@gmail.com>
2023-06-20 17:37:05 +0530

    testing

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ let jsonData = {};
 app.post('/Paymentsuccess', (req, res) => {
   jsonData = req.body
   console.log(req);
-  res.redirect("https://shop.incretech.in/Paymentsuccess");
+  res.redirect("https://shop.incretech.in//Paymentsuccess");
 
 });
 
@@ -107,7 +107,7 @@ app.post('/Paymentfailure', (req, res) => {
   jsonData = req.body
   console.log("jsonData", jsonData)
 
-  res.redirect('https://payumoney-d5dw.onrender.com/Paymentfailure');
+  res.redirect('https://shop.incretech.in/Paymentfailure');
 
 });
 
